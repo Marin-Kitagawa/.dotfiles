a3e79042d888516b7705d73374bc5f04e6596493
Marin Kitagawa <49131888+Marin-Kitagawa@users.noreply.github.com>
2024-03-01 14:19:09 +0530

    Add files via upload

---
+++ .wezterm.lua
@@ -0,0 +1,26 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+local mux = wezterm.mux
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+
+wezterm.on("gui-startup", function() 
+	local tab, pane, window = mux.spawn_window{}
+	window:gui_window():maximize()
+end)
+
+config = {
+	color_scheme = 'One Dark (Gogh)',
+	font = wezterm.font "FiraCode Nerd Font",
+	window_background_opacity = 0.8,
+}
+
+
+-- and finally, return the configuration to wezterm
+return config
